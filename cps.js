7e151a5e317275ae7ded14fb240b52fe386615ad
Jeree-Boiii <77164335+Jeree-Boiii@users.noreply.github.com>
2022-04-09 14:48:26 +0100

    Uploading files
    Just uploading some files from my computer into the repository.

---
+++ cps.js
@@ -0,0 +1,107 @@
+let button = document.getElementById("cps-button");
+let cpsLabel = document.getElementById("cps-label");
+let highestLabel = document.getElementById("highest");
+let speedLabel = document.getElementById("speed-display");
+
+var clicks = 0;
+var highestCps = 0;
+
+const crashSound = document.getElementById("crash");
+
+const slowMusic = document.getElementById("slow-music");
+const moderateMusic = document.getElementById("moderate-music");
+const fastishMusic = document.getElementById("fastish-music");
+const kindaFastMusic = document.getElementById("kinda-fast-music");
+const fastMusic = document.getElementById("fast-music");
+const superFastMusic = document.getElementById("super-fast-music");
+
+slowMusic.loop = true;
+moderateMusic.loop = true;
+fastishMusic.loop = true;
+kindaFastMusic.loop = true;
+fastMusic.loop = true;
+superFastMusic.loop = true;
+
+button.addEventListener("click", function(e){
+    clicks++
+    button.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}`;
+    e.preventDefault();
+});
+
+setInterval(cpsDisplay, 1000);
+function cpsDisplay(){
+
+    if (clicks > highestCps){
+        highestCps = clicks;
+    };
+    
+    cpsLabel.innerText = `CPS: ${clicks}`;
+    highestLabel.innerText = `Highest CPS: ${highestCps}`
+
+    if (clicks === 0){
+        speedLabel.innerText = "Click on this thing I guess";
+        button.style.background = "white";
+    };
+
+    if (clicks >= 1 && clicks < 6){
+        slowMusic.play();
+        speedLabel.innerText = "Are you even trying?";
+    }
+    else{
+        slowMusic.pause();
+    };
+    
+    if (clicks >= 6 && clicks < 10){
+        fastishMusic.play();
+        speedLabel.innerText = "I see you're taking this nice and easy";
+    }
+    else{
+        fastishMusic.pause();
+    };
+
+    if (clicks >= 10 && clicks < 13){
+        moderateMusic.play();
+        speedLabel.innerText = "This is pretty fast";
+    }
+    else{
+        moderateMusic.pause();
+    };
+
+
+    if (clicks >= 13 && clicks < 16){
+        kindaFastMusic.play();
+        speedLabel.innerText = "You have become s p e e d";
+    }
+    else{
+        kindaFastMusic.pause();
+    };
+    
+    if (clicks >= 16 && clicks < 20){
+        fastMusic.play();
+        speedLabel.innerText = "SUPA SPEED ACTIVAAAAAATE";
+    }
+    else{
+        fastMusic.pause();
+    };
+
+    if (clicks >= 20 && clicks < 40){
+        superFastMusic.play();
+        speedLabel.innerText = "AAAAAAAAAAAAAAAAAAAAAAAAH";
+    }
+    else{
+        superFastMusic.pause();
+    };
+    
+    if (clicks >= 40){
+        speedLabel.innerText = "Error found: diagnosing problem...";
+        crashSound.play()
+
+        setTimeout(function(){
+            window.location.replace("autoclick.html");
+        }, 5000);
+        
+        
+    };
+
+    clicks = 0;
+};
