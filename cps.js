c645a34998f8d4e7c654f35846db96ead4292bce
Jeree-Boiii <77164335+Jeree-Boiii@users.noreply.github.com>
2022-04-09 22:05:38 +0100

    update cps.js
    just changing some stuff with the anti-autoclicker

--- cps.js
+++ cps.js
@@ -23,83 +23,100 @@ fastMusic.loop = true;
 superFastMusic.loop = true;
 
 button.addEventListener("click", function(e){
-    clicks++
-    button.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}`;
-    e.preventDefault();
+    if (!crashed){
+        clicks++
+        button.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}`;
+        e.preventDefault();
+    }
 });
+
+var crashed = false;
 
 setInterval(cpsDisplay, 1000);
 function cpsDisplay(){
 
-    if (clicks > highestCps){
-        highestCps = clicks;
-    };
-    
-    cpsLabel.innerText = `CPS: ${clicks}`;
-    highestLabel.innerText = `Highest CPS: ${highestCps}`
+    if (!crashed){    
 
-    if (clicks === 0){
-        speedLabel.innerText = "Click on this thing I guess";
-        button.style.background = "white";
-    };
-
-    if (clicks >= 1 && clicks < 6){
-        slowMusic.play();
-        speedLabel.innerText = "Are you even trying?";
-    }
-    else{
-        slowMusic.pause();
-    };
-    
-    if (clicks >= 6 && clicks < 10){
-        fastishMusic.play();
-        speedLabel.innerText = "I see you're taking this nice and easy";
-    }
-    else{
-        fastishMusic.pause();
-    };
-
-    if (clicks >= 10 && clicks < 13){
-        moderateMusic.play();
-        speedLabel.innerText = "This is pretty fast";
-    }
-    else{
-        moderateMusic.pause();
-    };
-
-
-    if (clicks >= 13 && clicks < 16){
-        kindaFastMusic.play();
-        speedLabel.innerText = "You have become s p e e d";
-    }
-    else{
-        kindaFastMusic.pause();
-    };
-    
-    if (clicks >= 16 && clicks < 20){
-        fastMusic.play();
-        speedLabel.innerText = "SUPA SPEED ACTIVAAAAAATE";
-    }
-    else{
-        fastMusic.pause();
-    };
-
-    if (clicks >= 20 && clicks < 30){
-        superFastMusic.play();
-        speedLabel.innerText = "AAAAAAAAAAAAAAAAAAAAAAAAH";
-    }
-    else{
-        superFastMusic.pause();
-    };
-    
-    if (clicks >= 30){
-        speedLabel.innerText = "Error found: diagnosing problem...";
-        crashSound.play()
-
-        setTimeout(function(){
-            window.location.replace("autoclick.html");
-        }, 5000);
+        if (clicks > highestCps){
+            highestCps = clicks;
+        };
         
+        cpsLabel.innerText = `CPS: ${clicks}`;
+        highestLabel.innerText = `Highest CPS: ${highestCps}`
+
+        if (clicks === 0){
+            speedLabel.innerText = "Click on this thing I guess";
+            button.style.background = "white";
+        };
+
+        if (clicks >= 1 && clicks < 6){
+            slowMusic.play();
+            speedLabel.innerText = "Are you even trying?";
+        }
+        else{
+            slowMusic.pause();
+        };
+        
+        if (clicks >= 6 && clicks < 10){
+            fastishMusic.play();
+            speedLabel.innerText = "I see you're taking this nice and easy";
+        }
+        else{
+            fastishMusic.pause();
+        };
+
+        if (clicks >= 10 && clicks < 13){
+            moderateMusic.play();
+            speedLabel.innerText = "This is pretty fast";
+        }
+        else{
+            moderateMusic.pause();
+        };
+
+
+        if (clicks >= 13 && clicks < 16){
+            kindaFastMusic.play();
+            speedLabel.innerText = "You have become s p e e d";
+        }
+        else{
+            kindaFastMusic.pause();
+        };
+        
+        if (clicks >= 16 && clicks < 20){
+            fastMusic.play();
+            speedLabel.innerText = "SUPA SPEED ACTIVAAAAAATE";
+        }
+        else{
+            fastMusic.pause();
+        };
+
+        if (clicks >= 20 && clicks < 30){
+            superFastMusic.play();
+            speedLabel.innerText = "AAAAAAAAAAAAAAAAAAAAAAAAH";
+        }
+        else{
+            superFastMusic.pause();
+        };
+        
+        if (clicks >= 30){
+            speedLabel.innerText = "Error found: diagnosing problem...";
+            speedLabel.style.color = "red";
+
+            button.style.backgroundColor = "black";
+            button.style.color = "red";
+            button.innerText = "this is the end for you buddy";
+
+            document.body.style.backgroundColor = "black";
+
+            crashSound.play()
+
+            setTimeout(function(){
+                window.location.replace("autoclick.html");
+            }, 5000);
+
+            crashed = true;
+        
+    }
         
     };
 
